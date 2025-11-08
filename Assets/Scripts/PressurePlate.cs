a899f8aac69e476d95adb70baccac3ae16c463d2
yohitheyohi <103369772+yohitheyohi@users.noreply.github.com>
2025-11-08 21:04:26 +0100

    hilfe

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,31 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+     public UnityEvent myEvent = new UnityEvent();
+    void Update()
+    {
+        
+    }
+
+    public void DoMyEvent()
+    {
+        myEvent.Invoke();
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Blocks"))
+        {
+            bool isAttached = other.GetComponent<PushableObject>().isAttached;
+
+            if (!isAttached)
+            {
+                DoMyEvent();
+            }
+        }
+    }
+}
