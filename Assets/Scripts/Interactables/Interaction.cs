a8577c55c68af451834c889327974a1544ee8786
yohitheyohi <103369772+yohitheyohi@users.noreply.github.com>
2025-11-08 17:34:09 +0100

    PUSHABLE JD VANCE
    ALARM ALARM ALARM

---
+++ Assets/Scripts/Interactables/Interaction.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    private float horizontalPlayerInput = 0;
+    private float verticalPlayerInput = 0;
+    public LayerMask rayLayer;
+    public GameObject placeHolder;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalPlayerInput = Input.GetAxisRaw("Horizontal");
+        verticalPlayerInput = Input.GetAxisRaw("Vertical");
+        
+        
+
+        if (horizontalPlayerInput == verticalPlayerInput)
+        return;
+        
+        Vector2 playerInput = new Vector2(horizontalPlayerInput, verticalPlayerInput);
+        Vector3 playerPosition = new Vector3(transform.position.x + playerInput.x * 0.1f, transform.position.y + playerInput.y * 0.1f, -15f);
+        placeHolder.transform.position = playerPosition;
+        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(horizontalPlayerInput, verticalPlayerInput),1f, rayLayer );
+        Debug.DrawRay(transform.position, new Vector2(horizontalPlayerInput, verticalPlayerInput), Color.red);
+        if (hit)
+        {
+            print(hit.collider.name + " Hey im Here");
+            if(hit.collider != null && Input.GetKeyDown(KeyCode.E))
+            {
+                print(hit.collider.name + "Big Mama");
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    print(interactable.name);
+                    interactable.InvokeMyEvent();
+                }
+                   ;
+            }
+        }
+    }
+}
